5c5a95b76de2052389b45168c7588c85e0681c6e
rifqialfaizi <rifqialfaizii@gmail.com>
2020-09-02 08:43:59 +0700

    Initial Commit

---
+++ Let's Shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Let's Shop
+//
+//  Created by Rifqi Alfaizi on 02/09/20.
+//  Copyright © 2020 Rifqi Alfaizi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
